6f0d30350e78d52888f83c27a30eaf0430e3b2f0
Kradula <blockovichred@gmail.com>
2023-12-27 21:35:52 +0600

    created test_alerts_frame_windows.py
    added classes:
    -TestBrowserWindows
    added tests:
    -test_open_new_tab
    -test_open_new_window

---
+++ locators/alerts_frame_windows_page_locators.py
@@ -0,0 +1,9 @@
+
+
+class BrowserWindowsPageLocators:
+
+    NEW_TAB_BUTTON = ('xpath', "//button[@id='tabButton']")
+    NEW_WINDOW_BUTTON = ('xpath', "//button[@id='windowButton']")
+
+    TITLE_NEW = ('xpath', "//h1[@id='sampleHeading']")
+
