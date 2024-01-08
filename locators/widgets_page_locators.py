6e591eac776b8956ecd369d4d1cd56e4410f1e9c
Kradula <blockovichred@gmail.com>
2024-01-08 12:35:38 +0600

    added test_widgets.py, widgets_page.py, widgets_page_locators.py
    added classes:
    -TestAccordianPage
    added tests:
    -test_accordian

---
+++ locators/widgets_page_locators.py
@@ -0,0 +1,12 @@
+
+
+class AccordianPageLocators:
+
+    FIRST_SECTION = ('xpath', "//div[@id='section1Heading']")
+    FIRST_SECTION_CONTENT = ('xpath', "//div[@id='section1Content']/p")
+
+    SECOND_SECTION = ('xpath', "//div[@id='section2Heading']")
+    SECOND_SECTION_CONTENT = ('xpath', "//div[@id='section2Content']/p")
+
+    THIRD_SECTION = ('xpath', "section3Heading")
+    THIRD_SECTION_CONTENT = ('xpath', "//div[@id='section3Content']/p")
